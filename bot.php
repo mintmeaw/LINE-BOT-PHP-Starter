d016d445ca5918e4ce4b8c5e8d5ef629fb0c289f
mintmeaw <mintmeaw666@gmail.com>
2017-05-24 01:46:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,9 @@
-<?php
-echo "FGO";
+import random
+while True:
+    text = input("> ")
+    a = ['Rider','ไรด้า','ไรเดอร์']
+    b = ['http://fategrandorder.wikia.com/wiki/Rider']
+    if text in a:
+        print(random.choice(b))
+    else:
+        print("?")
